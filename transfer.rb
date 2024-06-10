abd6654ad1467102b0751f6cb09be5a7a38083f6
Francis Villanueva <326610+francis-villanueva@users.noreply.github.com>
2024-06-09 21:46:22 -0700

    feat(Import): second pass at script
    * wrapping text entry into quotes
    * adding debug statements for progress
      * output each entry date as it imports
      * number of imports it did with a percentage
    * a tiny method to calculate percentage

--- transfer.rb
+++ transfer.rb
@@ -1,38 +1,58 @@
-# read file
+# import required classes
+require 'time'
 
 # parse file into entries
 def parse_ohlife_export
 
-    export_text = File.read('./content/sample.txt')
+  # read file
+  export_text = File.read('./content/sample.txt')
 
-    # based on
-    # https://stackoverflow.com/questions/63628329/splitting-multiline-string-into-groups-based-on-a-regex-pattern
-    # Regex101 test: https://regex101.com/r/m6jMhE/1
-    parsed_entries = export_text.scan(/^\d.*(?:\r?\n(?!\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])\n\n).*)*/)
+  # based on
+  # https://stackoverflow.com/questions/63628329/splitting-multiline-string-into-groups-based-on-a-regex-pattern
+  # Regex101 test: https://regex101.com/r/m6jMhE/1
+  parsed_entries = export_text.scan(/^\d.*(?:\r?\n(?!\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])\n\n).*)*/)
+  parsed_entries_count = parsed_entries.size
+  entries_imported_count = 0
 
-    puts "Found #{parsed_entries.size} entries"
-    parsed_entries[0..0].each do |entry|
-        import_into_dayone(
-            entry_date: entry.match(/^\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])\n\n/), 
-            entry_text: entry.gsub!(/^\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])\n\n/, '')
-        )
-    end
+  puts "Found #{parsed_entries_count} entries"
+  parsed_entries.each do |entry|
+    imported = import_into_dayone(
+        entry_date: entry.match(/^\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])\n\n/)[0].strip,
+        entry_text: entry.gsub!(/^\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])\n\n/, '').strip
+    )
+
+    entries_imported_count += 1 if imported
+  end
+
+  puts "Import complete! #{entries_imported_count} entries imported: #{calculate_percent(entries_imported_count, parsed_entries_count)}%"
 end
 
-
+# call out to the DayOne CLI and import the entry
 def import_into_dayone(journal: "OhLife", timezone: "America/New_York", entry_date: nil, entry_text: "")
-    return false if entry_date.nil? || entry_text.empty?
-
+  if entry_date.nil? || entry_text.empty?
+    puts "\t Skipping entry..."
+    return false
+  else
     # Ex. 2015-06-01 15:53:10 using NOW
     # entry_date = Time.now.strftime("%Y-%m-%d %H:%M:%S")
 
-    puts "journal: #{journal}"
-    puts "timezone: #{timezone}"
+    # modify entry to account for timezone
+    # entry_date += "T23:59:00Z -4:00"
+
+    # puts "journal: #{journal}"
+    # puts "timezone: #{timezone}"
     puts "entry_date: #{entry_date}"
-    puts "entry_text: #{entry_text}"
+    # puts "entry_text: #{entry_text}"
     
-    import_cmd = "entry=\"#{entry_text}\" && dayone2 --journal='#{journal}' --time-zone='#{timezone}' --date='#{entry_date}' new $entry"
+    import_cmd = "entry=\"#{entry_text}\" && dayone2 --journal='#{journal}' --time-zone='#{timezone}' --date='#{entry_date}' new \"$entry\""
     %x[ #{import_cmd} ]
+    return true
+  end
+end
+
+# return a percentage rounded to 2 decimal places
+def calculate_percent(part, total)
+  ((part.to_f / total.to_f) * 100.0).round(2)
 end
 
 parse_ohlife_export()
